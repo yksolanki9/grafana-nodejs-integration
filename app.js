462ac8f2e3e42895ed3c5bdbbb5e84f0893b2550
Yash Solanki <yashsolankli1709@gmail.com>
2022-05-13 17:42:09 +0530

    Update panela and show it on going to a url

--- app.js
+++ app.js
@@ -1,60 +1,126 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
-const { addToken } = require('./middlewares/auth');
-const API_TOKEN = 'eyJrIjoiSmNMZ2d6dk9yVnIzRnhTdlkwbGhKSzJVNjMzM1g0UVMiLCJuIjoieWFzaHNvbCIsImlkIjoyfQ==';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/dashboards',
-  timeout: 1000,
-  
-});
-
-// app.get('/', (req, res) => {
-//   res.send('Haha GOTTYA');
-
-// });
+app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
   try {
+    const dbData = await axios.get(`http://localhost:3000/api/dashboards/uid/${process.env.DB_UID}`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.API_TOKEN}`
+      }
+    });
+
     const data = {
-      "dashboard": {
-        "id": null,
-        "uid": null,
-        "title": "Production Overview",
-        "tags": [ "templated" ],
-        "timezone": "browser",
-        "schemaVersion": 16,
-        "version": 0,
-        "refresh": "25s"
-      },
-      "folderId": 0,
-      "folderUid": "l3KqBxCMz",
-      "message": "Made changes to xyz",
-      "overwrite": false
+      "dashboard":  {
+        "id": dbData.data.dashboard.id,
+        "folderId": dbData.data.dashboard.folderId,
+        "panels": [
+            {
+              "id": 2,
+              "gridPos": {
+                "x": 0,
+                "y": 0,
+                "w": 12,
+                "h": 50
+              },
+              "type": "timeseries",
+              "title": "Testing this",
+              "options": {
+                "tooltip": {
+                  "mode": "single",
+                  "sort": "none"
+                },
+                "legend": {
+                  "displayMode": "list",
+                  "placement": "bottom",
+                  "calcs": []
+                }
+              },
+              "fieldConfig": {
+                "defaults": {
+                  "custom": {
+                    "drawStyle": "line",
+                    "lineInterpolation": "linear",
+                    "barAlignment": 0,
+                    "lineWidth": 10,
+                    "fillOpacity": 0,
+                    "gradientMode": "none",
+                    "spanNulls": false,
+                    "showPoints": "auto",
+                    "pointSize": 5,
+                    "stacking": {
+                      "mode": "none",
+                      "group": "A"
+                    },
+                    "axisPlacement": "auto",
+                    "axisLabel": "",
+                    "scaleDistribution": {
+                      "type": "linear"
+                    },
+                    "hideFrom": {
+                      "tooltip": false,
+                      "viz": false,
+                      "legend": false
+                    },
+                    "thresholdsStyle": {
+                      "mode": "off"
+                    }
+                  },
+                  "color": {
+                    "mode": "palette-classic"
+                  },
+                  "mappings": [],
+                  "thresholds": {
+                    "mode": "absolute",
+                    "steps": [
+                      {
+                        "value": null,
+                        "color": "green"
+                      },
+                      {
+                        "value": 80,
+                        "color": "red"
+                      }
+                    ]
+                  }
+                },
+                "overrides": []
+              },
+              "datasource": {
+                "uid": "grafana",
+                "type": "datasource"
+              }
+            }
+          ],
+        "title": dbData.data.dashboard.title,
+        "uid": dbData.data.dashboard.uid,
+        "version": dbData.data.dashboard.version
+      }
     }
-    const axiosRes = await axios.post('http://localhost:3000/api/dashboards/db', data, {
+
+    await axios.post('http://localhost:3000/api/dashboards/db', data, {
+      headers: {
+        'Authorization': `Bearer ${process.env.API_TOKEN}`
+      }
+    });
+    res.render('index', {IFRAME_URL: process.env.IFRAME_URL});
+  } catch(err) {
+    res.send(err);
+  }
+});
+
+app.get('/db', async(req, res) => {
+  try {
+    const axiosRes = await axios.get('http://localhost:3000/api/dashboards/uid/vf0C6gXnk', {
       headers: {'Authorization': `Bearer ${API_TOKEN}`}
     });
     res.send(axiosRes.data);
   } catch(err) {
     res.send(err);
   }
-});
-
-
-app.get('/db', async(req, res) => {
-  try {
-    const axiosRes = await axios.get('http://localhost:3000/api/dashboards/uid/2', {
-      headers: {'Authorization': `Bearer ${API_TOKEN}`}
-    });
-    res.send(axiosRes.data);
-  } catch(err) {
-    console.log(err);
-  }
 })
-
-
 
 app.listen(4000, () => console.log('Server running on port 4000'));
